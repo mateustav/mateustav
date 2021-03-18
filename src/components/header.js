a51cb9d541636cd9078283324fcfa5a591296e52
mateustav <mateustav@gmail.com>
2021-03-17 23:32:35 -0700

    Make the header look a lot better

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ import Img from "gatsby-image"
 const Header = () => (
   <StaticQuery
     query={graphql`query getHeaderImage {
-      mat: file(relativePath: { eq: "mateus.jpg" }) {
+      mat: file(relativePath: { eq: "16-bit-icon.png" }) {
         childImageSharp {
           fluid(maxWidth: 317) {
             ...GatsbyImageSharpFluid
@@ -16,16 +16,22 @@ const Header = () => (
     }
     `}
     render={data => (
-      <Container className="about-me py-sm-5">
-        <Row className="border align-items-center py-3 p-sm-5 position-relative h-100 justify-content-center justify-content-sm-between about-spotlight">
-          <Col md={6} className="mb-4 mb-sm-0">
-            <div>
-              <h1>Hi, I'm Mat!</h1>
-              <p className="mb-0">Full-time marketer &amp;<br/>web development enthusiast</p>
+      <Container className="py-sm-5">
+        <Row className="align-items-center justify-content-center">
+          <Col sm={11} md={12}>
+            <div className="header-card">
+              <Row className="align-items-start align-items-md-center pt-5 pt-sm-4 pt-lg-0 px-3 px-sm-5 position-relative h-100 justify-content-center justify-content-sm-between">
+                <Col sm={8} className="mb-4 mb-sm-0">
+                  <div>
+                    <h1 className="font-weight-lighter">Hi, I'm <span className="font-weight-bold">Mat</span>!</h1>
+                    <p className="mb-0 font-weight-bold">I'm a Full-Stack Developer based in Vancouver. 🇨🇦</p>
+                  </div>
+                </Col>
+                <Col sm={4} className="align-self-end">
+                  <Img fluid={data.mat.childImageSharp.fluid} alt="mat" className="about-me-headphoto" />
+                </Col>
+              </Row>
             </div>
-          </Col>
-          <Col md={6}>
-            <Img fluid={data.mat.childImageSharp.fluid} alt="test" className="about-me-headphoto" />
           </Col>
         </Row>
       </Container>
