acd52237fd8d7ba39bf2ac9121fcb69a3cfd6666
mateustav <mateustav@gmail.com>
2021-03-17 23:32:11 -0700

    Add Stack row to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/header"
+import Stack from "../components/stack"
 import SEO from "../components/seo"
 
 
@@ -11,6 +12,7 @@ const Home = ({
   <Layout pageInfo={{ pageName: "Home" }}>
     <SEO title="Hi, I'm Mat!" />
     <Header/>
+    <Stack />
   </Layout>
 )}
 
