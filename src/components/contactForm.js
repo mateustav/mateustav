7a2989f25caecacbd8b6525f5e26292853da42ee
mateustav <mateustav@gmail.com>
2021-03-21 12:13:37 -0700

    Move the contact form to its own component

---
+++ src/components/contactForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+import { Button, Form, Row, Col } from "react-bootstrap"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane, faSpinner } from '@fortawesome/pro-duotone-svg-icons'
+
+const ContactForm = () => {
+  
+  return (
+    <Form name="Contact Form" className="contact-form" method="POST" data-netlify="true">
+      <Row className="px-3 px-sm-0">
+        <Col md={7}>
+          <Form.Group controlId="name" className="pt-3">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Johnny Cash" />
+          </Form.Group>
+          <Form.Group controlId="email" className="py-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="johnny@maninblack.com" />
+          </Form.Group>
+          <Form.Group controlId="message" className="pb-3">
+            <Form.Label>Your Message</Form.Label>
+            <Form.Control as="textarea" rows="5" placeholder="Hello, I'm Johnny Cash..." /*placeholder="I keep a close watch on this heart of mine&#10;I keep my eyes wide open all the time..."*/ />
+          </Form.Group>
+          <Button variant="dark" type="submit" className="rounded-0 px-sm-4">
+            <FontAwesomeIcon icon={faPaperPlane} className="mr-2"/> Send
+          </Button>
+          <div class="submission-status mt-3"><FontAwesomeIcon icon={faSpinner} spin /> <span className="pl-2">Sending...</span>
+          </div>
+        </Col>
+      </Row>
+    </Form>
+)}
+
+export default ContactForm
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,9 @@
 import React from "react"
-import { Container, Row, Col, Form, Button } from "react-bootstrap"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane, faSpinner } from '@fortawesome/pro-duotone-svg-icons'
+import { Container, Row, Col } from "react-bootstrap"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ContactForm from "../components/contactForm"
 
 const ContactPage = () => (
   <Layout pageInfo={{ pageName: "contact-me" }}>
@@ -13,29 +12,7 @@ const ContactPage = () => (
       <Row>
         <Col>
           <h1 className="px-3 px-sm-0">Reach out!</h1>
-          <Form name="Contact Form  " className="contact-form" method="POST" data-netlify="true">
-            <Row className="px-3 px-sm-0">
-              <Col md={7}>
-                <Form.Group controlId="name" className="pt-3">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control type="text" placeholder="Johnny Cash" />
-                </Form.Group>
-                <Form.Group controlId="email" className="py-3">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="johnny@maninblack.com" />
-                </Form.Group>
-                <Form.Group controlId="message" className="pb-3">
-                  <Form.Label>Your Message</Form.Label>
-                  <Form.Control as="textarea" rows="5" placeholder="Hello, I'm Johnny Cash..." /*placeholder="I keep a close watch on this heart of mine&#10;I keep my eyes wide open all the time..."*/ />
-                </Form.Group>
-                <Button variant="dark" type="submit" className="rounded-0 px-sm-4">
-                  <FontAwesomeIcon icon={faPaperPlane} className="mr-2"/> Send
-                </Button>
-                <div class="submission-status mt-3"><FontAwesomeIcon icon={faSpinner} spin /> <span className="pl-2">Sending...</span>
-</div>
-              </Col>
-            </Row>
-          </Form>
+          <ContactForm />
         </Col>
       </Row>
       </Container>
