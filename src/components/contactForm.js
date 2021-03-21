238e989a2309c513eec197f5de4791766022b075
mateustav <mateustav@gmail.com>
2021-03-21 13:40:12 -0700

    Set state for submit status message

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,26 +1,30 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 import { Button, Form, Row, Col } from "react-bootstrap"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane, faSpinner } from '@fortawesome/pro-duotone-svg-icons'
 
 const ContactForm = () => {
   const form = useRef(null);
+  const [value, setValue] = useState({
+    submitFeedback: null
+  });
 
   return (
     <Form name="Contact Form" className="contact-form" method="POST" data-netlify="true" ref={form}>
       <Row className="px-3 px-sm-0">
         <Col md={7}>
+          <Form.Control ref="form-name" type="hidden" value="Contact Form"></Form.Control>
           <Form.Group controlId="name" className="pt-3">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Johnny Cash" required />
+            <Form.Control ref="name" type="text" placeholder="Johnny Cash" required />
           </Form.Group>
           <Form.Group controlId="email" className="py-3">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="johnny@maninblack.com" required />
+            <Form.Control ref="email" type="email" placeholder="johnny@maninblack.com" required />
           </Form.Group>
           <Form.Group controlId="message" className="pb-3">
             <Form.Label>Your Message</Form.Label>
-            <Form.Control as="textarea" rows="5" placeholder="Hello, I'm Johnny Cash..." /*placeholder="I keep a close watch on this heart of mine&#10;I keep my eyes wide open all the time..."*/ required />
+            <Form.Control ref="message" as="textarea" rows="5" placeholder="Hello, I'm Johnny Cash..." /*placeholder="I keep a close watch on this heart of mine&#10;I keep my eyes wide open all the time..."*/ required />
           </Form.Group>
           <Button variant="dark" type="submit" className="rounded-0 px-sm-4">
             <FontAwesomeIcon icon={faPaperPlane} className="mr-2"/> Send
