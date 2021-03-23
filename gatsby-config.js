16945fbffea6a9cbb3e8f8665f5dd6c59624863a
mateustav <mateustav@gmail.com>
2021-03-23 11:28:10 -0700

    Remove styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-webfonts`,
       options: {
