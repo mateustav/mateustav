a61ad557c13a6b4e092cf58d3f1112b78a5d5472
mateustav <mateustav@gmail.com>
2021-03-22 23:00:36 -0700

    Updates to the contact form logic

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -2,21 +2,31 @@ import * as qs from "query-string"
 import React, { useRef, useState } from "react"
 import { Button, Form, Row, Col } from "react-bootstrap"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane, faSpinner } from '@fortawesome/pro-duotone-svg-icons'
+import { faCheckCircle, faExclamationCircle, faPaperPlane, faSpinner } from '@fortawesome/pro-duotone-svg-icons'
 import axios from "axios"
 
 const ContactForm = () => {
   const form = useRef(null);
-  const formName = useRef('form-name');
   const name = useRef('name');
   const email = useRef('email');
   const message = useRef('message');
-  const [value, setValue] = useState({
-    submitFeedback: null
+  const [feedback, setValue] = useState({
+    submitFeedback: null,
+    submitIcon: faSpinner,
+    submitStatus: 'invisible'
   });
+
+  const {submitFeedback, submitIcon, submitStatus } = feedback;
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    setValue({
+      submitFeedback: "Sending...",
+      submitIcon: faSpinner,
+      submitStatus: "pending"
+    })
+
     const formData = {};
     const formInputs = new FormData(form.current);
     for (const entry of formInputs.entries()) {
@@ -33,15 +43,25 @@ const ContactForm = () => {
       axios(axiosOptions)
       .then(response => {
         setValue({
-          submitFeedback: "Form submitted successfully!",
+          submitFeedback: "Thanks! Your message was received.",
+          submitIcon: faCheckCircle,
+          submitStatus: "success"
         })
         form.current.reset();
       })
-      .catch(err =>
+      .catch(err => {
         setValue({
           submitFeedback: "Form could not be submitted.",
+          submitIcon: faExclamationCircle,
+          submitStatus: "error"
         })
-      )
+      })
+  };
+
+  const handleFocus = (e) => {
+    setValue({
+      submitStatus: 'invisible',
+    })
   };
 
   return (
@@ -51,20 +71,20 @@ const ContactForm = () => {
           <Form.Control type="hidden" value="Contact Form"></Form.Control>
           <Form.Group controlId="name" className="pt-3">
             <Form.Label>Name</Form.Label>
-            <Form.Control name="name" type="text" placeholder="Johnny Cash" required />
+            <Form.Control name="name" type="text" placeholder="Johnny Cash" ref={name} onFocus={e => handleFocus(e)} required />
           </Form.Group>
           <Form.Group controlId="email" className="py-3">
             <Form.Label>Email address</Form.Label>
-            <Form.Control name="email" type="email" placeholder="johnny@maninblack.com" required />
+            <Form.Control name="email" type="email" placeholder="johnny@maninblack.com" ref={email} onFocus={e => handleFocus(e)} required />
           </Form.Group>
           <Form.Group controlId="message" className="pb-3">
             <Form.Label>Your Message</Form.Label>
-            <Form.Control name="message" as="textarea" rows="5" placeholder="Hello, I'm Johnny Cash..." /*placeholder="I keep a close watch on this heart of mine&#10;I keep my eyes wide open all the time..."*/ required />
+            <Form.Control name="message" as="textarea" rows="5" placeholder="Hello, I'm Johnny Cash..." /*placeholder="I keep a close watch on this heart of mine&#10;I keep my eyes wide open all the time..."*/ ref={message} onFocus={e => handleFocus(e)} required />
           </Form.Group>
-          <Button variant="dark" type="submit" className="rounded-0 px-sm-4">
+          <Button variant="dark" type="submit" className="rounded-0 px-sm-4" onFocus={e => handleFocus(e)}>
             <FontAwesomeIcon icon={faPaperPlane} className="mr-2"/> Send
           </Button>
-          <div className="submission-status mt-3"><FontAwesomeIcon icon={faSpinner} spin /> <span className="pl-2">Sending...</span>
+          <div className={`submission-status mt-3 ${submitStatus}`}><FontAwesomeIcon icon={submitIcon} /> <span className="pl-2">{submitFeedback}</span>
           </div>
         </Col>
       </Row>
