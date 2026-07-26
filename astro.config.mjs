5b12aa5be4d73424293f9345443e3d83b00d25a7
Mateus Tavares <mateustav@gmail.com>
2026-07-26 14:05:59 -0600

    Configure trailingSlash: always in Astro and add public/_redirects for Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mattavares.com',
+  trailingSlash: 'always',
   output: 'static',
   integrations: [sitemap(), mdx()],
   image: {
